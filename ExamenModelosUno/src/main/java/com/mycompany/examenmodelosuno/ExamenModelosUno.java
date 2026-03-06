9572363f42b1f2c14259f62b434d7d50ef263c11
Jo.-el <joelperez555888@gmail.com>
2026-03-06 14:38:30 -0500

    Primera version examen

---
+++ ExamenModelosUno/src/main/java/com/mycompany/examenmodelosuno/ExamenModelosUno.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examenmodelosuno;
+
+/**
+ *
+ * @author yisus
+ */
+public class ExamenModelosUno {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
